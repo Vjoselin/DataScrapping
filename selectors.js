dd0050f82829524bfe964b6c7fe79086dd7baed0
Joselin <48998341+Vjoselin@users.noreply.github.com>
2021-01-21 16:51:41 +0530

    Update selectors.js

--- selectors.js
+++ selectors.js
@@ -1,5 +1,9 @@
-document.querySelector(".pdate"); 
-document.querySelector(".bdate"); 
-document.querySelector(".obj"); 
-document.querySelectorAll("menu-item"); 
+var element1 =document.querySelector(".pdate");
+var element2 =document.querySelector(".bdate");
+var element3 =document.querySelector(".obj");
+var element4 =document.querySelector(".menu");
 
+console.log(element1);
+console.log(element2);
+console.log(element3);
+console.log(element4);
